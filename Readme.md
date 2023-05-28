9164e1f1114074b75500e83e97cb82f9e601c3c1
WeimusT <wei.david.tai@gmail.com>
2023-05-28 20:31:02 +0100

    KDJ Implementation done

--- Readme.md
+++ Readme.md
@@ -7,7 +7,10 @@ Most trading strategies are adapted from the book [Quantitative Trading: How to 
 - Kelly Formula: Portofolio management.
 - Pair trading: Trade ETF spread. 
 - Sharpe Ratio: Sharpe ratio is a ratio of expected return and risk. Some simple strategies such as buy and hold are implemented to explain Sharpe ratio.
-- Statistical Factor Model: Use Principal Component Regression (PCR) to explain the variation expected return prediction. 
+- Statistical Factor Model: Use Principal Component Regression (PCR) to explain the variation expected return prediction.
+- KDJ
+
+![KDJ TENB](res/TENB_KDJ.png)
 
 # Todo
 - Pair trade [NASDAQ BUG](https://www.globalxetfs.com/funds/bug/) ETF and S&P 500.
@@ -18,3 +21,4 @@ Most trading strategies are adapted from the book [Quantitative Trading: How to 
 [Factor Models and Principal Component Analysis](http://www.fsb.miamioh.edu/lij14/672_2014_s10.pdf)
 
 [Analysis of Financial Time Series](https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiZmtf95on_AhXNilwKHeIADt0QFnoECAoQAQ&url=https%3A%2F%2Fcpb-us-w2.wpmucdn.com%2Fblog.nus.edu.sg%2Fdist%2F0%2F6796%2Ffiles%2F2017%2F03%2Fanalysis-of-financial-time-series-copy-2ffgm3v.pdf&usg=AOvVaw3guhNjv9ee67BeV-s7VPC7)
+
--- src/strategies.py
+++ src/strategies.py
@@ -6,10 +6,12 @@ import os.path
 import sys
 import logging
 from argparse import ArgumentParser
+from matplotlib import pyplot as plt
 
 import backtrader as bt
 
 logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 LOG_LEVEL = logging.DEBUG
 
 class KDJStrategy(bt.Strategy):
@@ -54,7 +56,7 @@ class KDJStrategy(bt.Strategy):
                 )
                 self.buyprice = order.executed.price
                 self.buycomm = order.executed.comm
-                self.bar_executed_close = self.dataclose[0]
+                self.bar_executed_close = self.data_close[0]
             else:
                 self.logger.info(
                     "SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f"
@@ -71,7 +73,7 @@ class KDJStrategy(bt.Strategy):
         self.logger.info("OPERATION PROFIT, GROSS %.2f, NET %.2f" % (trade.pnl, trade.pnlcomm))
     
     def next(self):
-        self.logger.info("Close, %.2f" % self.dataclose[0])
+        self.logger.info("Close, %.2f" % self.data_close[0])
         if self.order:
             return
         condition1 = self.J[-1] - self.D[-1]
@@ -79,19 +81,38 @@ class KDJStrategy(bt.Strategy):
         if not self.position:
             # J - D
             if condition1 < 0 and condition2 > 0:
-                self.logger.info("BUY CREATE, %.2f" % self.dataclose[0])
+                self.logger.info("BUY CREATE, %.2f" % self.data_close[0])
                 self.order = self.buy()
         else:
             if condition1 > 0 or condition2 < 0:
-                self.logger.info("SELL CREATED. %.2f" % self.dataclose[0])
+                self.logger.info("SELL CREATED. %.2f" % self.data_close[0])
                 self.order = self.sell()
 
-def run(strategy, data):
+def run(strategy, dataname):
     cerebro = bt.Cerebro()
     cerebro.addstrategy(strategy)
-    data = bt.feeds.GenericCSVData.GenericCSVData
+    data = bt.feeds.GenericCSVData(
+        dataname=dataname,
+        dtformat='%Y-%m-%d %H:%M:%S%z',
+        datetime=0,
+        time=-1,
+        open=1,
+        high=2,
+        low=3,
+        close=4,
+        volume=5
+    )
+    cerebro.adddata(data)
+    cerebro.broker.setcash(10000)
+    cerebro.addsizer(bt.sizers.FixedSize, stake=100)
+    cerebro.broker.setcommission(commission=5e-3)
+    logger.info("START PORTFOLIO VALUE: %.2f" % cerebro.broker.getvalue())
+    cerebro.run()
+    logger.info("FINAL PORTFOLIO VALUE: %.2f" % cerebro.broker.getvalue())
+    cerebro.plot()
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--data", type=str)
-    run()
+    parser.add_argument("--dataname", type=str)
+    args = parser.parse_args()
+    run(KDJStrategy, args.dataname)
